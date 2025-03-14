5d3e0809cb5729ddfbe48c8f3dd81327c1938b96
Bbaranoski <breno.b.o.baranoski@gmail.com>
2025-03-14 16:35:46 -0300

    aprendendo como funciona o layout e tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-blue-500 text-white text-2xl font-bold">
-      Tailwind está funcionando! 
+    <div className="flex h-full min-w-screen items-center justify-center bg-blue-300 text-white text-2xl font-bold">
+      <h1 >
+        Hello World
+      </h1>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-blue-500 p-4 text-white">
+            <h1 className="text-x1 font-bold">ToDo</h1>
+        </nav>
+    )
+}
