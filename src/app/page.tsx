4cf00f5f72987eb2fcb7cb5af3b4fe3bd794b9cd
Bbaranoski <breno.b.o.baranoski@gmail.com>
2025-03-28 13:44:32 -0300

    fazendo com que a array apareca no layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,36 +7,43 @@ export default function Home() {
     descricao: string;
   }
   const [todo, setTodo] = useState<Todo>({titulo: "", descricao: ""});
-  const lista: Todo[] = [{titulo: "Teste", descricao: "teste 2"}];
+  const [lista, setLista] = useState<Todo[]>([{titulo: "Teste", descricao: "teste 2"}]);
 
-  useEffect(() => {
-    {lista.map((e, index) => (
-      <div key={index} className="bg-blue-200 p-4 rounded-md">
-        <p>{e.titulo}</p>
-        <p>{e.descricao}</p>
-      </div>
-    ))}
-  }, [lista]);
+  const addTodo = () => {
+    if(todo.titulo !== "" && todo.descricao !== "") {
+      setLista([...lista, todo]);
+      setTodo({titulo: "", descricao: ""});
+    }
+  }
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] w-full gap-4 p-4">
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] grid-rows-[repeat(auto-fit,minmax(250,1fr))] w-full gap-3 p-3">
+      {lista.map((e, index) => (
+        <div key={index} className="bg-blue-200 p-4 rounded-md">
+          <p>{e.titulo}</p>
+          <p>{e.descricao}</p>
+        </div>
+      ))}
         <div className="bg-purple-500 p-4 rounded-md order-2">
-          <input type="text" 
-          value={todo.titulo}
-          onChange={(e) => setTodo({...todo, titulo: e.target.value})}
+          <input type="text"
+            placeholder="Título"
+            className="p-2 border rounded mb-2 w-full" 
+            value={todo.titulo}
+            onChange={(e) => setTodo({...todo, titulo: e.target.value})}
           />
-          <input type="text" 
-          value={todo.descricao}
-          onChange={(e) => setTodo({...todo, descricao: e.target.value})}
+          <input type="text"
+            placeholder="Descrição"
+            className="p-2 border rounded mb-2 w-full"
+            value={todo.descricao}
+            onChange={(e) => setTodo({...todo, descricao: e.target.value})}
           />
-          <input type="date" />
-          <button>X</button>
-          <button onClick={() => {console.log(lista) 
-              if(todo.titulo != ''){
-                lista.push(todo)
-              }
-            }}
-          >V</button>
+          <input type="date" className="p-2 border rounded mb-2 w-full"/>
+          <div className="flex gap-2">
+            <button className="bg-red-500 text-white px-4 py-2 rounded">X</button>
+            <button className="bg-green-500 text-white px-4 py-2 rounded"
+              onClick={addTodo}
+            >V</button>
+          </div>
         </div>
     </div>
   );
