a6f5afa891ccca23eb28c83298b62ca124927957
BBaranoski <breno.b.o.baranoski@gmail.com>
2025-06-10 14:19:59 -0300

    criando o service e adicionando a tabela User com prisma

--- back/back/src/auth/auth.module.ts
+++ back/back/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/todo/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
-// import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
--- back/back/src/auth/auth.service.ts
+++ back/back/src/auth/auth.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException, BadRequestException } from '@nestjs/common';
+import { PrismaService } from 'src/todo/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private prisma: PrismaService,
+        private jwtService: JwtService,
+    ) {}
+
+    async register(data: RegisterDto) {
+        //valida o email
+        const existingUser = await this.prisma.user.findUnique({
+            where: { email: data.email }
+        })
+        if (existingUser) {
+            throw new BadRequestException('Email já cadastrado')
+        }
+        //cria hash na senha
+        const hashedPassword = await bcrypt.hash(data.password, 10)
+        //cria o usuario no banco
+        const user = await this.prisma.user.create({
+            data: {
+                name: data.name,
+                email: data.email,
+                password: hashedPassword,
+            },
+        })
+
+        const { password, ...userWithoutPassword } = user
+        return userWithoutPassword
+    }
+
+    async validateUser(email: string, password: string) {
+        //valida o email
+        const user = await this.prisma.user.findUnique({
+            where: { email },
+        })
+        if(!user){
+            throw new UnauthorizedException('Usuário não encontrado')
+        }
+        //valida a senha
+        const passwordValid = await bcrypt.compare(password, user.password)
+        if(!passwordValid){
+            throw new UnauthorizedException('Senha incorreta')
+        }
+
+        const { password: _, ...userWithoutPassword } = user
+        return userWithoutPassword
+    }
+
+    async login(user: { id: string; email: string }){
+        const payload = { sub: user.id, email: user.email }
+
+        const token = await this.jwtService.signAsync(payload)
+        return{
+            access_token: token,
+            expires_in: '1h',
+        }
+    }
+}
