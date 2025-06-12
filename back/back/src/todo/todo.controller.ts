ccc913ec67e9db5ce5a959d5f9270f761fdafa5e
BBaranoski <breno.b.o.baranoski@gmail.com>
2025-06-12 13:57:24 -0300

    adicionando relacao nas tabelas do prisma e configurando authguard no todo.controller

--- back/back/src/todo/todo.controller.ts
+++ back/back/src/todo/todo.controller.ts
@@ -1,41 +1,43 @@
-import { Controller, Get, Post, Body, Delete, Param, Patch, UsePipes, ValidationPipe, Query } from "@nestjs/common"
+import { Controller, Get, Post, Body, Delete, Param, Patch, UsePipes, ValidationPipe, UseGuards, Req } from "@nestjs/common"
 import { TodoService } from "./todo.services"
 import { CreateTodoDto } from "./dto/create-todo.dto"
 import { UpdateTodoDto } from "./dto/update-todo.dto"
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard"
 
+@UseGuards(JwtAuthGuard)
 @Controller("todos")
 export class TodoController {
     constructor(private readonly todoService: TodoService) {}
 
     @Post()
     @UsePipes(new ValidationPipe({whitelist: true}))
-    create(@Body() data: CreateTodoDto) {
-        console.log('Tua mae é bixa')
-        return this.todoService.create(data)
+    create(@Req() req, @Body() data: CreateTodoDto) {
+        return this.todoService.create(req.user.userId, data)
     }
 
     @Post('filtro')
-    filter(@Body() filtos: any) {
-        return this.todoService.filtrar(filtos)
+    filter(@Req() req, @Body() filtos: any) {
+        return this.todoService.filtrar(req.user.userId, filtos)
     }
 
     @Get()
-    findAll() {
-        console.log('Breno é bixa')
-        return this.todoService.findAll()
+    findAll(@Req() req) {
+        return this.todoService.findAll(req.user.userId)
     }
 
     @Delete(":id")
-    delete(@Param("id") id: string) {
-        return this.todoService.delete(Number(id))
+    delete(@Req() req, @Param("id") id: string) {
+        return this.todoService.delete(req.user.userId, Number(id))
     }
 
     @Patch(":id")
     @UsePipes(new ValidationPipe({whitelist: true}))
     update(
+        @Req() req,
         @Param("id") id: string,
         @Body() data: UpdateTodoDto
     ){
-        return this.todoService.update(Number(id), data)
+        
+        return this.todoService.update(req.user.userId, Number(id), data)
     }
 }
